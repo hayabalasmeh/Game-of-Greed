a189d97fa955f07221a5144da57ba1f7490c3bd4
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-08-02 00:49:20 +0300

    calc score

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -1,11 +1,19 @@
 from random import sample
+from collections import Counter
 
 
-class GameLogic:
-    
+class GameLogic:   
     @staticmethod
-    def calculate_score():
-        pass
+    def calculate_score(d):
+        score = 0
+        #The Counter() function returns unorderd dictionary
+        d = Counter(d)
+        if len(d) == 0:
+            return 0
+        # most_common function returns a list, which is sorted based on the count of the elements.
+        elif d.most_common(1) == 1 and len(d) == 6:
+            score += 1500
+            return score
     @staticmethod
     def roll_dice(num):
         return sample(range(1,6+1),num)
@@ -18,6 +26,5 @@ class Banker:
         pass 
     def clear_shelf():
         pass
-    
     
     
