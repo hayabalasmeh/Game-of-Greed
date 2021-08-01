36ecc196818860df359f9a930d2b4a6425c33117
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-08-01 17:15:18 +0300

    adding trials

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -1,11 +1,12 @@
 from random import sample
-
+from collections import Counter
 
 class GameLogic:
     
     @staticmethod
     def calculate_score():
         pass
+        
     @staticmethod
     def roll_dice(num):
         return sample(range(1,6+1),num)
@@ -19,5 +20,13 @@ class Banker:
     def clear_shelf():
         pass
     
-    
+if __name__== "__main__":
+    items = (1,2,2,3,3,4) # 
+    result = Counter(items)
+    dic = {(1,1):100,(2,2):50}
+    score = 0
+    for item in result.most_common():
+       score = score + dic.get(item,0)
+       
+    print(score)
     
