c9a9feaab235e74e0be7eadc60fb6efb5f09cc38
Samer Odeh <samerodeh220@gmail.com>
2021-08-02 14:28:21 +0300

    merge to pull the code

--- tests/test_roll_dice.py
+++ tests/test_roll_dice.py
@@ -5,7 +5,8 @@
 - The output of `roll_dice` is a tuple with random values between 1 and 6.
 - The length of tuple must match the argument given to `roll_dice` method.
 """
-import pytest
+import pytest 
+
 from game_of_greed.game_logic import GameLogic
 
 pytestmark = [pytest.mark.version_1]
